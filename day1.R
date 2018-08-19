f235996e47621973a054530fb7eb5a1be0b4c522
mahesh90969 <mahesh.p17142@iimtrichy.ac.in>
2018-08-19 19:18:13 +0530

    DAY_1

--- day1.R
+++ day1.R
@@ -1,3 +1,10 @@
-############        DAY 1         ##########
+############        DAY 1         #############
 
 mtcars
+
+
+git config --global user.email "mahesh.p17142@iimtrichy.ac.in"
+git config --global user.name "mahesh90969"
+
+
+women
